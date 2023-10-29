09cd42e859d5ac599b81d5eee88598f368f4e1c4
John <101512527+Y0hn@users.noreply.github.com>
2023-10-29 20:45:59 +0100

    Room Generation Modularity
    Blueprint LRDU for all rooms
    + U D L R path blocks
    
    Better Random Wall spawning

--- Scripts/SpawnObject.cs
+++ Scripts/SpawnObject.cs
@@ -8,8 +8,31 @@ public class SpawnTile : MonoBehaviour
 
     void Start()
     {
-        objects = GameObject.FindGameObjectsWithTag(transform.name);
-        int rand = Random.Range(0, objects.Length);
-        Instantiate(objects[rand], transform.position, Quaternion.identity, transform);
+        GameObject spawn;
+
+        if (transform.name.Contains("Wall"))
+        {
+            List<GameObject> temp = new List<GameObject>();
+
+            string[] s = transform.name.Split('-');
+            objects = GameObject.FindGameObjectsWithTag(s[0]);
+
+            foreach (GameObject obj in objects)
+                if (obj.name.Contains(s[1]))
+                {
+                    spawn = obj;
+                    temp.Add(obj);
+                }
+            int rand = Random.Range(0, temp.Count);
+            spawn = temp[rand];
+        }
+        else
+        {
+            objects = GameObject.FindGameObjectsWithTag(transform.name);
+            int rand = Random.Range(0, objects.Length);
+            spawn = objects[rand];
+        }
+
+        Instantiate(spawn, transform.position, Quaternion.identity, transform);
     }
 }
