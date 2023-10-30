6246463b928350387ed567af6d8c60f71e99ed0d
John <101512527+Y0hn@users.noreply.github.com>
2023-10-30 23:10:14 +0100

    Main Menu tepmlate
    placeholder picture for now

--- Scripts/MainMenuManager.cs
+++ Scripts/MainMenuManager.cs
@@ -1,20 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    private int width, height;
-    private bool Full = false;
+    public string ver;
+
+    private Transform mainMenu;
+    private Transform setMenu;
+    private bool main;
 
     void Start()
     {
+        mainMenu = GameObject.FindGameObjectWithTag("Melee Enemy").GetComponent<Transform>();
+        setMenu = GameObject.FindGameObjectWithTag("Ranged Enemy").GetComponent<Transform>();
+        GameObject.FindGameObjectWithTag("Finish").transform.GetComponent<Text>().text += ver;
 
+        main = true;
+        mainMenu.gameObject.SetActive(main);
+        setMenu.gameObject.SetActive(!main);
     }
     void Update()
     {
 
     }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void EnterExitSettings()
+    {
+        main = !main;
+        mainMenu.gameObject.SetActive(main);
+        setMenu.gameObject.SetActive(!main);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    /*
     public void SetWidth(int newWidth)
     {
         width = newWidth;
@@ -31,4 +58,5 @@ public class MenuManager : MonoBehaviour
     {
         Screen.SetResolution(width, height, Full);
     }
+    */
 }
