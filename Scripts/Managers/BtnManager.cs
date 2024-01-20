319af210bdf8a5932a7c64ee6d47e4fbea54bb9d
John <101512527+Y0hn@users.noreply.github.com>
2024-01-20 23:58:38 +0100

    Sub Menu animation
    + Shader
    + BtnManager

---
+++ Scripts/Managers/BtnManager.cs
@@ -0,0 +1,148 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnManager : MonoBehaviour
+{
+    public RectTransform[] menus;
+
+    private Dictionary<string, RectTransform> buttonsRect;
+    private Dictionary<string, Button> buttons;
+    private Dictionary<string, int> menusNames;
+    private Dictionary<int, Vector2> menusPos;
+    private Dictionary<string, int> menuBtns;
+
+    // Animation
+    private const float animationDur = 50f;
+    public float animationSpeed = 50f;
+    private RectTransform animated;
+    private Vector2 targetPos;
+    private AlfaShade shader;
+    private bool deactivate;
+    private bool animate;
+    private bool moveX;
+
+    void Start()
+    {
+        // References
+        buttons = new();
+        menuBtns = new();
+        menusPos = new();
+        menusNames = new();
+        buttonsRect = new();
+        for (int i = 0; i < menus.Length; i++)
+        {
+            RectTransform child = menus[i];
+            List<Transform> grandChildren = new();
+            for (int j = 0; j < child.childCount; j++)
+                grandChildren.Add(child.GetChild(j));
+            foreach (Transform grandChild in grandChildren)
+            {
+                try
+                {
+                    if (grandChild.name.Contains("B"))
+                        grandChild.name = grandChild.name.Split('B')[0];
+                    buttonsRect.Add(grandChild.name, grandChild.GetComponent<RectTransform>());
+                    buttons.Add(grandChild.name, grandChild.GetComponent<Button>());
+                    menuBtns.Add(grandChild.name, i);
+                } catch { Debug.LogWarning("GrandChild: " + grandChild.name + " is not a button or is not compatible"); }
+            }
+            menusNames.Add(child.name, i);
+            menusPos.Add(i, child.position);
+
+            // Btn Setup
+            child.position = new(- child.position.x - child.sizeDelta.x, child.position.y);
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (animate)
+        {
+            if (
+                ((animated.position.x < targetPos.x || animated.position.y < targetPos.y) && 0 < animationSpeed)
+                    ||
+                ((animated.position.x > targetPos.x || animated.position.y > targetPos.y) && 0 > animationSpeed)
+                )
+            {
+                if (moveX)
+                    animated.position = new(animated.position.x + animationSpeed * Time.deltaTime, animated.position.y);
+                else
+                    animated.position = new(animated.position.x, animated.position.y + animationSpeed * Time.deltaTime);
+
+                // Shading
+                animated.GetComponent<AlfaShade>().AddTransparency(1/(animationDur/(animationSpeed * Time.deltaTime)));
+            }
+            else
+            {
+                animated.gameObject.SetActive(!deactivate);
+                animate = false;
+            }
+        }
+    }
+    public void DisableBtn(string name)
+    {
+        try
+        {
+            buttons[name].interactable = false;
+            Debug.Log("Disabled btn: " + name);
+        } 
+        catch 
+        {
+            string btns = "";
+
+            string[] temp = { "" };
+            if (buttonsRect != null)
+                temp = buttonsRect.Keys.ToArray();
+            if (!temp[0].Equals(string.Empty))
+
+                foreach (string key in temp)
+                {
+                    btns += "\n" + key;
+                }
+            else
+                btns = "[ is empty ]";
+
+            Debug.LogWarning("Button " + name + " is not in Dictionaries (yet)\nList of Buttons:" + btns);
+        }
+    }
+    public void EnDisMenu(string name)
+    {
+        int index = menusNames[name];
+        animated = menus[index];
+        shader = animated.GetComponent<AlfaShade>();
+        moveX = true;
+
+        if (animated.gameObject.activeSelf)
+        {
+            animationSpeed = -Mathf.Abs(animationSpeed);
+            if (moveX)
+                targetPos = new(animated.position.x - animationDur, animated.position.y);
+            else
+                targetPos = new(animated.position.x, animated.position.y - animationDur);
+
+            Debug.Log("Disambling " + name);
+            shader.SetTransparency(1f);
+            deactivate = true;
+        }
+        else
+        {
+            animationSpeed = Mathf.Abs(animationSpeed);
+            targetPos = menusPos[index];
+
+            if (moveX)
+                animated.position = new(targetPos.x - animationDur, animated.position.y);
+            else
+                animated.position = new(targetPos.x, animated.position.y - animationDur);
+            animated.gameObject.SetActive(true);
+
+            Debug.Log("Enabling " + name);
+            shader.SetTransparency(0f);
+            deactivate = false;
+        }
+
+        animate = true;
+    }
+}
--- Scripts/UI/MainMenuManager.cs
+++ Scripts/UI/MainMenuManager.cs
@@ -7,15 +7,15 @@ public class MenuManager : MonoBehaviour
 {
     public Transform titleScreen;
     public Transform mainMenu;
-    public Transform setMenu;
 
     private RectTransform tScreen;
+    private BtnManager btnManager;
     private Image mScreen;
-    private RectTransform sScreen;
 
     private readonly static float[] titleZoomParam = { 10f, -260f, 3780f };
     private const float scaleChange = 2f;
     private float[] moveBy = new float[2];
+    private bool inMenu;
     private bool main;
     private bool sub;
 
@@ -26,8 +26,8 @@ public class MenuManager : MonoBehaviour
         Transform comp = GameObject.FindGameObjectWithTag("Floor").transform;
         Transform name = GameObject.FindGameObjectWithTag("Tile").transform;
         tScreen = titleScreen.GetComponent<RectTransform>();
+        btnManager = mainMenu.GetComponent<BtnManager>();
         mScreen = mainMenu.GetComponent<Image>();
-        sScreen = setMenu.GetComponent<RectTransform>();
 
         // Set Up
         vers.GetComponent<Text>().text += Application.version;
@@ -35,73 +35,85 @@ public class MenuManager : MonoBehaviour
         comp.GetComponent<Text>().text = Application.companyName;
 
         // Starting Values for variables
+        sub = true;
         main = true;
-        sub = false;
         moveBy[0] = 0;
         moveBy[1] = 0;
-        setMenu.gameObject.SetActive(false);
         mainMenu.gameObject.SetActive(false);
         titleScreen.gameObject.SetActive(true);
-        mainMenu.GetChild(0).GetChild(0).GetComponent<Button>().interactable = SaveCheck();
-
     }
     void Update()
     {
-        if (main)
+        if (inMenu)
         {
-            if (Input.GetKeyDown(KeyCode.Escape))
+            // Tranzitions
+            if (main)
             {
-                Quit();
+
             }
-            else if (Input.anyKeyDown)
+            else // if (sub)
             {
-                main = false;
-                sub = false;
+
             }
         }
-        else if (!sub)
+        else // Try to focus Menu
         {
-            if (moveBy[0] == 0)
+            if (main)
             {
-                int counter = (int)Math.Ceiling((titleZoomParam[0] - 1) / scaleChange);
-                moveBy[0] = titleZoomParam[1] / counter;
-                moveBy[1] = titleZoomParam[2] / counter;
-                //Debug.Log($"Counter setted to {counter}");
+                if (Input.GetKeyDown(KeyCode.Escape))
+                    Quit();
+                else if (Input.anyKeyDown)
+                    main = false;
             }
-            else if (tScreen.localScale.x <= titleZoomParam[0])//if (Time.time <= timer) //if (tScreen.localScale.x < titleZoomParam[0])
+            else if (sub)
             {
-                float x = tScreen.position.x + moveBy[0] * Time.deltaTime;
-                float y = tScreen.position.y + moveBy[1] * Time.deltaTime;
-                tScreen.position = new Vector3(x, y, 0);
+                if (moveBy[0] == 0)
+                {
+                    int counter = (int)Math.Ceiling((titleZoomParam[0] - 1) / scaleChange);
+                    moveBy[0] = titleZoomParam[1] / counter;
+                    moveBy[1] = titleZoomParam[2] / counter;
+                    //Debug.Log($"Counter setted to {counter}");
+                }
+                else if (tScreen.localScale.x <= titleZoomParam[0])//if (Time.time <= timer) //if (tScreen.localScale.x < titleZoomParam[0])
+                {
+                    float x = tScreen.position.x + moveBy[0] * Time.deltaTime;
+                    float y = tScreen.position.y + moveBy[1] * Time.deltaTime;
+                    tScreen.position = new Vector3(x, y, 0);
 
-                x = tScreen.localScale.x + scaleChange * Time.deltaTime;
-                y = tScreen.localScale.y + scaleChange * Time.deltaTime;
-                tScreen.localScale = new Vector3(x, y, 1);
+                    x = tScreen.localScale.x + scaleChange * Time.deltaTime;
+                    y = tScreen.localScale.y + scaleChange * Time.deltaTime;
+                    tScreen.localScale = new Vector3(x, y, 1);
+                }
+                else
+                {
+                    //Debug.Log("Zoomed");
+                    mScreen.color = new Color(mScreen.color.r, mScreen.color.g, mScreen.color.b, 0);
+                    mainMenu.gameObject.SetActive(true);
+                    sub = false;
+                }
             }
             else
             {
-                //Debug.Log("Zoomed");
-                mScreen.color = new Color(mScreen.color.r, mScreen.color.g, mScreen.color.b, 0);
-                mainMenu.gameObject.SetActive(true);
-                sub = true;
-            }
-        }
-        else // sub = true
-        {
-            if (mScreen.color.a < 1)
-            {
-                float newA = mScreen.color.a + Time.deltaTime;
-                if (newA > 1)
-                    newA = 1;
+                if (mScreen.color.a < 1)
+                {
+                    float newA = mScreen.color.a + Time.deltaTime;
+                    if (newA > 1)
+                        newA = 1;
 
-                //Debug.Log($"Color a set to {newA}");
-                mScreen.color = new Color(mScreen.color.r, mScreen.color.g, mScreen.color.b, newA);
-            }
-            else
-            {
-                titleScreen.gameObject.SetActive(false);
-                tScreen.localScale = new Vector3(1, 1, 1);
-                tScreen.position = Vector3.zero;
+                    //Debug.Log($"Color a set to {newA}");
+                    mScreen.color = new Color(mScreen.color.r, mScreen.color.g, mScreen.color.b, newA);
+                }
+                else
+                {
+                    titleScreen.gameObject.SetActive(false);
+                    tScreen.localScale = new Vector3(1, 1, 1);
+                    tScreen.position = Vector3.zero;
+                    inMenu = true;
+                    main = true;
+                    if (!SaveCheck())
+                        btnManager.DisableBtn("Continue");
+                    btnManager.EnDisMenu("Main");
+                }
             }
         }
     }
@@ -115,17 +127,15 @@ public class MenuManager : MonoBehaviour
     }
     public void EnterExitSettings()
     {
-        main = !main;
-        mainMenu.gameObject.SetActive(main);
-        setMenu.gameObject.SetActive(!main);
+        btnManager.EnDisMenu("Settings");
     }
     public void Quit()
     {
         Application.Quit();
     }
-
     private bool SaveCheck()
     {
+        // Does its magic here
         return false;
     }
 }
